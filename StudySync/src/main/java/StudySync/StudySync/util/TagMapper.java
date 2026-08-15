ecfcbd2aed606c41a2b0126d0ab6c734f50fdc14
Nguyen Toan Thang <xamthang09@gmail.com>
2026-08-15 12:35:25 +0700

    fix(ec2): edit ports container study backend

--- StudySync/src/main/java/StudySync/StudySync/util/TagMapper.java
+++ StudySync/src/main/java/StudySync/StudySync/util/TagMapper.java
@@ -6,7 +6,7 @@ import StudySync.StudySync.domain.response.TagResponse;
 public final class TagMapper {
 
     private TagMapper() {}
-
+ 
     public static TagResponse toResponse(Tag tag) {
         return TagResponse.builder()
                 .id(tag.getId())
